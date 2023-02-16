b8c22668e6e72d6c0d720cebfdb5d67725dce53e
urdaddygh <aziret.2000@bk.ru>
2023-02-16 15:57:46 +0600

    first commit

---
+++ Templates/script.js
@@ -0,0 +1,13 @@
+let dropIcon = document.querySelector('.header_profile__img');
+let dropDown = document.querySelector('.dropdown');
+console.log(dropDown)
+dropIcon.addEventListener('click', function(e){
+    dropDown.classList.toggle('active')
+})
+
+let select = document.querySelector('.select');
+let sort = document.querySelector('.sort');
+
+select.addEventListener('click', function(e){
+    sort.classList.toggle('sort_active')
+})
